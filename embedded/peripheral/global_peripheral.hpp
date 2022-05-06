d5439eb1e12fba1bb5ac75b4e1fcf06df55e24da
UnnamedOrange <lycheng1215@sina.com>
2022-05-06 16:13:27 +0800

    使用设计的管脚
    TODO: 检查设计的管脚

--- embedded/peripheral/global_peripheral.hpp
+++ embedded/peripheral/global_peripheral.hpp
@@ -18,57 +18,57 @@
  *
  * @note 在具体硬件上修改该常量。
  *
- * @todo 使用设计的管脚。
+ * @todo 检查设计的管脚。
  */
-inline constexpr PinName PIN_BC26_TX = USBTX;
+inline constexpr PinName PIN_BC26_TX = PA_2;
 /**
  * @brief BC26 的 RX（CPU 端）管脚。
  *
  * @note 在具体硬件上修改该常量。
  *
- * @todo 使用设计的管脚。
+ * @todo 检查设计的管脚。
  */
-inline constexpr PinName PIN_GPS_RX = USBRX;
+inline constexpr PinName PIN_BC26_RX = PA_3;
 /**
  * @brief GPS 的 TX（CPU 端）管脚。
  *
  * @note 在具体硬件上修改该常量。
  *
- * @todo 使用设计的管脚。
+ * @todo 检查设计的管脚。
  */
-inline constexpr PinName PIN_GPS_TX = USBTX;
+inline constexpr PinName PIN_GPS_TX = PC_4;
 /**
  * @brief GPS 的 RX（CPU 端）管脚。
  *
  * @note 在具体硬件上修改该常量。
  *
- * @todo 使用设计的管脚。
+ * @todo 检查设计的管脚。
  */
-inline constexpr PinName PIN_BC26_RX = USBRX;
+inline constexpr PinName PIN_GPS_RX = PC_5;
 /**
  * @brief 加速度计的 MOSI 管脚。
  *
  * @note 在具体硬件上修改该常量。
  *
- * @todo 使用设计的管脚。
+ * @todo 检查设计的管脚。
  */
-inline constexpr PinName PIN_ACCEL_MOSI = NC;
+inline constexpr PinName PIN_ACCEL_MOSI = PA_7;
 /**
  * @brief 加速度计的 MISO 管脚。
  *
  * @note 在具体硬件上修改该常量。
  *
- * @todo 使用设计的管脚。
+ * @todo 检查设计的管脚。
  */
-inline constexpr PinName PIN_ACCEL_MISO = NC;
+inline constexpr PinName PIN_ACCEL_MISO = PA_6;
 /**
  * @brief 加速度计的 SCLK 管脚。
  *
  * @note 在具体硬件上修改该常量。
  *
- * @todo 使用设计的管脚。
+ * @todo 检查设计的管脚。
  */
-inline constexpr PinName PIN_ACCEL_SCLK = NC;
+inline constexpr PinName PIN_ACCEL_SCLK = PA_5;
 /**
  * @brief 加速度计的 CS 管脚。
  *
