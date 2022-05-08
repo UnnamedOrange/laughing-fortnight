49c9a6628f50f089772397488f8504c5ed0c0a84
UnnamedOrange <lycheng1215@sina.com>
2022-05-08 20:37:01 +0800

    核对管脚
    Co-Authored-By: zhiquanxu <71125069+zhiquanxu@users.noreply.github.com>

--- embedded/peripheral/global_peripheral.hpp
+++ embedded/peripheral/global_peripheral.hpp
@@ -13,34 +13,33 @@
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wc++17-extensions"
+
 /**
  * @brief BC26 的 TX（CPU 端）管脚。
- *
- * @note 在具体硬件上修改该常量。
  */
 inline constexpr PinName PIN_BC26_TX = PA_9;
 /**
  * @brief BC26 的 RX（CPU 端）管脚。
- *
- * @note 在具体硬件上修改该常量。
  */
 inline constexpr PinName PIN_BC26_RX = PA_10;
+
+/**
+ * @brief GPS 的电源使能管脚。
+ *
+ * @todo 检查是高使能还是低使能。
+ */
+inline constexpr PinName PIN_GPS_EN = PB_3;
 /**
  * @brief GPS 的 TX（CPU 端）管脚。
- *
- * @note 在具体硬件上修改该常量。
  */
 inline constexpr PinName PIN_GPS_TX = PC_4;
 /**
  * @brief GPS 的 RX（CPU 端）管脚。
- *
- * @note 在具体硬件上修改该常量。
  */
 inline constexpr PinName PIN_GPS_RX = PC_5;
+
 /**
  * @brief 加速度计的 MOSI 管脚。
- *
- * @note 在具体硬件上修改该常量。
  *
  * @todo 检查设计的管脚。
  */
@@ -48,15 +47,11 @@ inline constexpr PinName PIN_ACCEL_MOSI = PA_7;
 /**
  * @brief 加速度计的 MISO 管脚。
  *
- * @note 在具体硬件上修改该常量。
- *
  * @todo 检查设计的管脚。
  */
 inline constexpr PinName PIN_ACCEL_MISO = PA_6;
 /**
  * @brief 加速度计的 SCLK 管脚。
- *
- * @note 在具体硬件上修改该常量。
  *
  * @todo 检查设计的管脚。
  */
@@ -64,10 +59,38 @@ inline constexpr PinName PIN_ACCEL_SCLK = PA_5;
 /**
  * @brief 加速度计的 CS 管脚。
  *
- * @note 在具体硬件上修改该常量。
- *
- * @todo 使用设计的管脚。
+ * @todo 检查设计的管脚。
  */
-inline constexpr PinName PIN_ACCEL_CS = NC;
+inline constexpr PinName PIN_ACCEL_CS = PC_11;
+/**
+ * @brief 加速度计的中断 1 管脚。
+ *
+ * @todo 检查设计的管脚。
+ */
+inline constexpr PinName PIN_ACCEL_INT1 = PC_10;
+/**
+ * @brief 加速度计的中断 2 管脚。
+ *
+ * @todo 检查设计的管脚。
+ */
+inline constexpr PinName PIN_ACCEL_INT2 = PC_12;
+
+/**
+ * @brief 蜂鸣器的电源使能管脚。是 5 V 稳压模块的使能。
+ *
+ * @todo 检查是高使能还是低使能。
+ */
+inline constexpr PinName PIN_BUZZER_EN = PA_8;
+/**
+ * @brief 蜂鸣器的输出管脚。
+ */
+inline constexpr PinName PIN_BUZZER = PB_10;
+
+/**
+ * @brief LED 灯的管脚。
+ *
+ * @todo 检查是高使能还是低使能。
+ */
+inline constexpr PinName PIN_LED = PB_5;
 
 #pragma GCC diagnostic pop
