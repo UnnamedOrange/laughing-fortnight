4ca1e5ac59ae013e910b58581f9b9e12793dea91
UnnamedOrange <lycheng1215@sina.com>
2022-05-10 18:18:50 +0800

    更新标准框架的测试

--- embedded/test/peripheral/test_peripheral_std_framework.hpp
+++ embedded/test/peripheral/test_peripheral_std_framework.hpp
@@ -23,6 +23,7 @@ namespace test
      * @brief 测试 peripheral_std_framework。
      * - 测试能否正常收发消息。
      * - 测试能否在子线程内向自己 push 消息。（是可以的）
+     * - 测试子类销毁时消息处理函数能否正确执行。
      */
     class test_peripheral_std_framework
     {
@@ -33,8 +34,19 @@ namespace test
         };
         class _fake_peripheral : public peripheral::peripheral_std_framework
         {
+        private:
+            std::vector<int> test_exit{1, 2, 3};
+
+        public:
+            ~_fake_peripheral()
+            {
+                descendant_exit();
+            }
+
+        private:
             void on_message(int id, std::shared_ptr<void> data) override
             {
+                descendant_callback_begin();
                 switch (id)
                 {
                 case 0:
@@ -70,7 +82,25 @@ namespace test
                     push(0, nullptr);
                     break;
                 }
+                case 4:
+                {
+                    utils::debug_printf("[-] id 4\n");
+
+                    using namespace std::literals;
+                    // 睡眠 3 s，如果不加锁，子类的析构函数已经完毕，
+                    // 子类的成员变量 test_exit 也被销毁。
+                    rtos::ThisThread::sleep_for(3s);
+
+                    // 加锁后，子类的对象未被销毁，可以使用。
+                    if (test_exit.size() == 3 && test_exit[0] == 1 &&
+                        test_exit[1] == 2 && test_exit[2] == 3)
+                        utils::debug_printf("[D] id 4\n");
+                    else
+                        utils::debug_printf("[F] id 4\n");
+                    break;
                 }
+                }
+                descendant_callback_end();
             }
         } _fp;
 
@@ -95,12 +125,13 @@ namespace test
             // 测试能否在子线程内向自己 push 消息。
             _fp.push(3, nullptr);
 
-            // 等待子线程输出结束后再输出提示信息。
-            rtos::ThisThread::sleep_for(1s);
-            // 收到警告是正常现象。
-            utils::debug_printf(
-                "[I] The warning 1 second later can be ignored.\n");
-            // 之后的延迟来源于 peripheral_std_framework 的析构函数。
+            // 确保以上消息处理完毕。
+            rtos::ThisThread::sleep_for(500ms);
+
+            // 测试能否等待消息处理结束后再析构。
+            _fp.push(4, nullptr);
+            // 确保开始处理该消息。
+            rtos::ThisThread::sleep_for(10ms);
         }
     };
 } // namespace test
